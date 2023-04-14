ca549e2c2adadd8d9e7203baa7fe882a66a71f5b
Juliano Liberato <juliano@liberato.eti.br>
2023-04-13 22:35:07 -0300

    Create cleanfiles-asterisk.sh

---
+++ cleanfiles-asterisk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#julianol1berato - aula 2 YT.
+
+##clean files, not used 
+echo "" > func_odbc.conf
+echo "" > cdr_pgsql.conf
+echo "" > cdr_odbc.conf 
+echo "" > cdr_manager.con
+echo "" > cdr_custom.conf
+echo "" > cdr_beanstalkd.conf
+echo "" > cel_custom.conf
