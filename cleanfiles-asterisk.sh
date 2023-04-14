b77f7580bc8f0d6c2303b8dfdd4fa76b414791dc
Juliano Liberato <juliano@liberato.eti.br>
2023-04-13 22:52:51 -0300

    Update cleanfiles-asterisk.sh

--- cleanfiles-asterisk.sh
+++ cleanfiles-asterisk.sh
@@ -2,10 +2,11 @@
 #julianol1berato - aula 2 YT.
 
 ##clean files, not used 
-echo "" > func_odbc.conf
-echo "" > cdr_pgsql.conf
-echo "" > cdr_odbc.conf 
-echo "" > cdr_manager.con
-echo "" > cdr_custom.conf
-echo "" > cdr_beanstalkd.conf
-echo "" > cel_custom.conf
+echo "" > /etc/asterisk/func_odbc.conf
+echo "" > /etc/asterisk/cdr_pgsql.conf
+echo "" > /etc/asterisk/cdr_odbc.conf 
+echo "" > /etc/asterisk/cdr_manager.con
+echo "" > /etc/asterisk/cdr_custom.conf
+echo "" > /etc/asterisk/cdr_beanstalkd.conf
+echo "" > /etc/asterisk/cel_custom.conf
+echo "" > /etc/asterisk/cel_pgsql.conf
