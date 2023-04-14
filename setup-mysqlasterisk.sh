7965c0db9f095d457c9ffa498e076249a1e9de28
L1berato <juliano@liberato.eti.br>
2023-04-14 13:56:28 -0300

    add

---
+++ setup-mysqlasterisk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+##julianol1berato install mysql for asterisk20lts, auto-configure.
+
+
+apt install -y unixodbc odbcinst mariadb-client mariadb-server odbc-mariadb -y
+systemctl enable mariadb
+systemctl start mariadb
+
+apt install python3-pip -y
+pip install alembic
+pip install psycopg2-binary
+pip install mysql-connector-python
+apt install python3-pymysql/stable python3-mysqldb/stable
+pip install mysqlclient
+pip install "importlib_metadata==1.5.2"
+pip install "zipp==1.2.0"
+pip install "configparser==3.8.1"
+
+cd /usr/src/asterisk/contrib/ast-db-manage
+mv config.ini.sample config.ini
