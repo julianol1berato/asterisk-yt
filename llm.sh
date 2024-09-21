7ee76b261cb650bb3de1c30821172797d40eb6f9
Juliano Liberato <juliano@9level.com.br>
2024-09-21 13:04:00 -0300

    Update llm.sh

--- llm.sh
+++ llm.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
 apt install mpg123 sox libjsonld-perl libjson-perl libio-socket-ip-perl libio-socket-ssl-perl libio-socket-timeout-perl perl flac libencode-perl libfile-temp-perl libfile-path-perl libdigest-md5-perl
+cpan LWP::UserAgent && cpan JSON
