a8bb4b0f498bc035dadceefa380cbcfd86feb647
Juliano Liberato <juliano@9level.com.br>
2024-09-21 13:02:58 -0300

    Create llm.sh

---
+++ llm.sh
@@ -0,0 +1,1 @@
+apt install mpg123 sox libjsonld-perl libjson-perl libio-socket-ip-perl libio-socket-ssl-perl libio-socket-timeout-perl perl flac libencode-perl libfile-temp-perl libfile-path-perl libdigest-md5-perl
