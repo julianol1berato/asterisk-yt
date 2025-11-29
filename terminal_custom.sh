ef40aed05a5764f8a7639a97c9e67abeb50ec06b
Juliano Liberato <juliano@9level.com.br>
2025-11-29 12:36:01 -0300

    Update terminal_custom.sh
    remove custom fields terminal

--- terminal_custom.sh
+++ terminal_custom.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-## julianol1berato - projects debian
-## customizações de terminal e log
+## julianol1berato
 
 apt update && apt upgrade -y
 apt install wget curl vim atop htop nmap whowatch mtr htop rsync acl locate ethtool screen lshw hdparm ttyrec tcpdump auditd sudo openssh-server libsox-fmt-base dialog unzip autoconf -y
-
-
-echo 'HISTTIMEFORMAT="%d/%m/%Y - %T => "' >> ~/.bashrc
-echo "HISTSIZE=50000" >> ~/.bashrc
-echo "HISTFILESIZE=100000" >> ~/.bashrc
-echo "shopt -s histappend" >> ~/.bashrc
-echo "export HISTCONTROL=ignoredups:erasedups" >> ~/.bashrc
-cp ~/.bashrc{,.orig}
-echo "PS1='\[\033[00;32m\]┌─[\[\033[00;31m\]\u\[\033[00m\]@\[\033[01;34m\]\h\[\033[00;32m\]]──[\[\033[01;33m\]\A\[\033[00;32m\]]──[\[\033[00m\]\w\[\033[00;32m\]]\n└─[\!]─>\$\[\033[00;32m\] '" >> ~/.bashrc
